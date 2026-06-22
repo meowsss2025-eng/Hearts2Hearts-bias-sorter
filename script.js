79576fb5f7a258a24183a73d67fe97a0a85343f1
meowsss2025-eng <meowsss2025@gmail.com>
2026-06-22 13:20:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const members = [
-  { name:"Jiwoo",  img:"https://n.uguu.se/zAeXfxGV.jpg" },
-  { name:"Carmen", img:"https://h.uguu.se/VwyeYehH.jpg" },
-  { name:"Yuha",   img:"https://n.uguu.se/wDKplObL.jpg" },
-  { name:"Stella", img:"https://h.uguu.se/suZlEogG.jpg" },
-  { name:"Juun",   img:"https://d.uguu.se/ReOLAxLA.jpg" },
-  { name:"A-na",   img:"https://o.uguu.se/imEuEZfb.jpg" },
-  { name:"Ian",    img:"https://d.uguu.se/ohEqNLSa.jpg" },
-  { name:"Ye-on",  img:"https://d.uguu.se/bkrTdzdY.jpg" }
+  { name:"Jiwoo",  img:"https://drive.google.com/file/d/1IDd3jpJsbaIVs0i2UJCChFLyNQsOKgLL/view?usp=drivesdk" },
+  { name:"Carmen", img:"https://drive.google.com/file/d/1dCdwHC_sGf9HWCCHFuD11Oik-6K1U6_g/view?usp=drivesdk" },
+  { name:"Yuha",   img:"https://drive.google.com/file/d/1zf4jJhN8wZVhqaFw0SzcGikAQMsbej_d/view?usp=drivesdk" },
+  { name:"Stella", img:"https://drive.google.com/file/d/128HKaKHdOObi5sSZk9tXUTpAzgwzuY42/view?usp=drivesdk" },
+  { name:"Juun",   img:"https://drive.google.com/file/d/1h7XWMo56wxu1oD3SeoRNmML-zxaX-eUS/view?usp=drivesdk" },
+  { name:"A-na",   img:"https://drive.google.com/file/d/1IguHeqJgza76jiLli6q6a0pOuKJuUuuO/view?usp=drivesdk" },
+  { name:"Ian",    img:"https://drive.google.com/file/d/1nVXzZnelQA9zm9cpdXbt1rYcsR0eDKxO/view?usp=drivesdk" },
+  { name:"Ye-on",  img:"https://drive.google.com/file/d/1ck98-ZnjA-nI8PrClovh2Dk0dWT_3Edj/view?usp=drivesdk" }
 ];
 
 let scores = {};
